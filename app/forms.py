01be3a82cb9d126fc69192c08d9c0f319e04fa11
David Shin <shin.david93@gmail.com>
2022-01-18 11:50:42 -0800

    Add flask-wtf and then create RegisterForm

---
+++ app/forms.py
@@ -0,0 +1,12 @@
+from ast import Sub
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_pass = PasswordField('Confirm Password', validator=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
