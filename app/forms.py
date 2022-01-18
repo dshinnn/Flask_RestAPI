ecfcac3755946984185ddf91972959fb40a7faac
David Shin <shin.david93@gmail.com>
2022-01-18 12:55:02 -0800

    Add form

--- app/forms.py
+++ app/forms.py
@@ -5,8 +5,8 @@ from wtforms.validators import DataRequired, Email, EqualTo
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_pass = PasswordField('Confirm Password', validator=[DataRequired(), EqualTo('password')])
+    confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,25 @@
 from app import app
 from flask import render_template
+from app.forms import RegisterForm
 
 @app.route('/')
 def index():
-    my_name = 'David'
     colors = ['red', 'blue', 'green']
     person = {
         'name': 'Ferris Buller',
         'age': 18,
         'best_friend': 'Cameron'
     }
-    return render_template('index.html', name = 'David', city='Arcadia', colors=colors, person=person)
+    return render_template('index.html', name='David', city='Arcadia', colors=colors, person=person)
 
 
 @app.route('/name')
 def name():
-    my_name = 'David'
     return render_template('name.html', name='David')
 
-
-@app.route('/test')
-def test_name():
-    return '<h1>This is a test</h1>'
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        print('Form has been validated!')
+    return render_template('register.html', form=form)
