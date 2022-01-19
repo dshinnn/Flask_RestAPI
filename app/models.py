e2c7708c6e2d56155708099c6c99ed34da2e8630
David Shin <shin.david93@gmail.com>
2022-01-19 09:17:11 -0800

    Model for user and flask-migrate install

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from app import db
+import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.password = generate_password_hash(kwargs['password'])
+
+    def __repr__(self):
+        return f'<User|{self.username}'
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
