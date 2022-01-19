2726a24833537dcdcd8e94de0b46027ee8663f5b
David Shin <shin.david93@gmail.com>
2022-01-19 13:05:46 -0800

    Flask-login set up

--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
-from app import db
+from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+@login.user_loader
+def get_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
