d49db6a6c152bdfba91c1e05309629a5938f7b79
David Shin <shin.david93@gmail.com>
2022-01-19 08:53:47 -0800

    Add sqlalchemy package and set up config

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you will never guess'
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 from . import routes
